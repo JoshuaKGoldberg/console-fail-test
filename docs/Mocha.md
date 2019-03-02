8a10e2680b8fe101e564ca318619ccc680e26287
Josh Goldberg <josh@fullscreenmario.com>
2019-03-02 17:18:10 -0500

    Spruced up docs with a support table

---
+++ docs/Mocha.md
@@ -0,0 +1,20 @@
+# Jest
+
+Jest is supported as a testing framework.
+It will be auto-detected if available.
+
+## Setup
+
+Include this as your first file before including other tests:
+
+```shell
+mocha ./node_modules/console-fail-test/setup.js src/**/*.test.js
+```
+
+Alternately, if you have a setup file already being run first, you'd like to manually enable this in individual files, you can use the Node API:
+
+```js
+// some.test.js
+
+require("console-fail-test").ctf();
+```
--- src/environments/allTestEnvironments.ts
+++ src/environments/allTestEnvironments.ts
@@ -2,7 +2,12 @@ import { getJestEnvironment } from "./jest";
 import { getMochaEnvironment } from "./mocha";
 import { TestEnvironmentGetter } from "./testEnvironmentTypes";
 
-const allTestEnvironmentGetters: TestEnvironmentGetter[] = [getJestEnvironment, getMochaEnvironment];
+const allTestEnvironmentGetters: TestEnvironmentGetter[] = [
+    getJestEnvironment,
+    // Mocha should be last because it's difficult to accurately detect
+    // See https://github.com/RyzacInc/console-fail-test/issues/10
+    getMochaEnvironment,
+];
 
 export const getTestEnvironment = () => {
     for (const testEnvironmentGetter of allTestEnvironmentGetters) {
