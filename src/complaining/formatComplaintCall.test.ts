a4cdf3453bc17a3b90196e1cf5b154f474364e41
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-05-06 08:13:46 -0400

    fix: don't crash when printing recursive objects (#208)

--- src/complaining/formatComplaintCall.test.ts
+++ src/complaining/formatComplaintCall.test.ts
@@ -7,6 +7,14 @@ describe("formatComplaintCall", () => {
     [[true], "true"],
     [[{}], "{}"],
     [[{ inner: {} }], '{"inner":{}}'],
+    [
+      (() => {
+        const x = { prop: {} };
+        x.prop = x;
+        return [x];
+      })(),
+      "A recursive object with keys: prop",
+    ],
     [[1, "two", [3]], '1, "two", [3]'],
   ])(`formats %p as %p`, (args, expected) => {
     const actual = formatComplaintCall(args);
