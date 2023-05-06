6eed4acc601cf2d9dbefcce14e0743c36bc69c8d
Josh Goldberg <git@joshuakgoldberg.com>
2023-05-06 08:07:16 -0400

    ignore vitest.config.js

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("vitest/config");
-exports.default = (0, config_1.defineConfig)({
-  test: {
-    clearMocks: true,
-    exclude: ["lib", "node_modules", "src/**/*.js", "src/*/*.js"],
-  },
-});
